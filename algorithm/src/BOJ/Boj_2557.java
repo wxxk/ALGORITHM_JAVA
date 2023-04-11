2e30ddd1d999816caee2fd64e6558629498b809c
wxxk <dwde22@gmail.com>
2023-04-11 18:40:37 +0900

    BOJ_2557

--- algorithm/src/BOJ/Boj_2557.java
+++ algorithm/src/BOJ/Boj_2557.java
@@ -1,10 +1,9 @@
-package algorithm;
+package BOJ;
 
 public class Boj_2557 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Hello World!");
 	}
 
 }
