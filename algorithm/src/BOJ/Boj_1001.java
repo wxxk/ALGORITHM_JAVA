5168e26021410f5000dd598e6038a04828c544c2
wxxk <dwde22@gmail.com>
2023-04-11 18:51:52 +0900

    BOJ_1001

---
+++ algorithm/src/BOJ/Boj_1001.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Boj_1001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		int result = a - b;
+		System.out.println(result);
+	}
+
+}
