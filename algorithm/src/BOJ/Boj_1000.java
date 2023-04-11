54c58cf727701f1f01d71fef29d337780ce7ddae
wxxk <dwde22@gmail.com>
2023-04-11 18:44:01 +0900

    BOJ_1000

---
+++ algorithm/src/BOJ/Boj_1000.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Boj_1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int result = a + b;
+		System.out.println(result);
+		
+	}
+
+}
